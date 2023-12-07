c6f47966a68775db1424c759e320f40ed6ea3872
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-07 22:43:56 +0900

    [feat] NotFound 추가

---
+++ src/components/jsx/NotFound.jsx
@@ -0,0 +1,14 @@
+function NotFound() {
+  return (
+    <div className="text-center">
+      <h1>
+        <b>404</b>
+      </h1>
+      <h1>
+        <b>Not Found</b>
+      </h1>
+    </div>
+  );
+}
+
+export default NotFound;
