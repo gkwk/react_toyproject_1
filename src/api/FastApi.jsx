8c936290bc5071f95984acd583fc76becd01adb4
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-08 21:34:58 +0900

    [feat] api 호출 구현

---
+++ src/api/FastApi.jsx
@@ -0,0 +1,53 @@
+
+function FastApi(method, url, params, success_callback, failure_callback) {
+  const contentType = 'application/json';
+  const body = JSON.stringify(params);
+  const rootDomain = process.env.REACT_APP_FASTAPI_ROOT_DOMAIN;
+  const port = process.env.REACT_APP_FASTAPI_PORT;
+
+  var targetUrl = rootDomain + ":" + port;
+  var parameters = {
+    "method" : method,
+    "headers" : {
+      "Content-Type" : contentType
+    }
+  }
+
+  if (url.startsWith("/")) {
+    targetUrl += url;
+  }else {
+    targetUrl += "/" + url;
+  }
+  
+  if(method.toUpperCase() === "GET") {
+    if (params !== null) {
+      targetUrl += "?" + params;
+    }
+  }else{
+    parameters["body"] = body;
+  }
+
+  fetch(targetUrl, parameters)
+  .then((response) => {
+    response.json()
+      .then((json) => {
+        var newJson = json;
+        newJson["status"] = response.status;
+        if(response.status >= 200 && response.status < 300) {
+          if(success_callback) {
+            success_callback(newJson);
+          }
+        }else {
+          if (failure_callback) {
+            failure_callback(newJson)
+          }else {
+            alert(JSON.stringify(newJson));
+          }
+        }
+      }).catch((error) => {
+        alert(JSON.stringify(error));
+      })
+  })
+}
+
+export default FastApi;
