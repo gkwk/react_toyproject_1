af03b352416d8fdd7a51102838c81e53a80fb4e2
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-08 21:35:36 +0900

    [feat] 라우터에 home 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import NotFound from './components/jsx/NotFound.jsx';
+import Home from './components/jsx/Home.jsx';
 
 function App() {
   const router = createBrowserRouter([
+    {
+      path: '/',
+      Component: Home,
+    },
     {
       path: '*',
       Component: NotFound,
