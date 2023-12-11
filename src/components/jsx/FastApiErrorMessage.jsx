e06d0960ba509c8ccf2b8988383d45c4c1bf47de
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-11 19:21:42 +0900

    [feat] 에러메세지 출력 처리 기능 추가

---
+++ src/components/jsx/FastApiErrorMessage.jsx
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+
+function FastApiErrorMessage(error) {
+  function errorMessageListing() {
+    if (Object.hasOwn(error, "detail")) {
+      if (typeof error.detail === "string") {
+        return (
+          <Fragment>
+            <li>
+              {error.detail}
+            </li>
+          </Fragment>
+        )
+      } else if (typeof error.detail === 'object' && error.detail.length > 0 ) {
+        return (
+          Object.keys(error.detail).map((index)=>{
+            return (
+              <Fragment key={index}>
+                <li>
+                  {error.detail[index].loc[1] + " : " + error.detail[index].msg}
+                </li>
+              </Fragment>
+            )
+          })
+        )
+      }
+    }
+  }
+
+  return (
+    <Fragment>
+      <ul className="list-unstyled">
+        {errorMessageListing()}
+      </ul>
+    </Fragment>
+  )
+}
+
+export default FastApiErrorMessage;
