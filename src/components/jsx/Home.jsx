a0cd3954928ec8092e2391bb4d3d3fe71f5580ad
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-11 19:23:00 +0900

    [feat] create를 위한 form 추가 및 에러메세지 출력 추가

--- src/components/jsx/Home.jsx
+++ src/components/jsx/Home.jsx
@@ -2,23 +2,25 @@ import { useState, useEffect, Fragment } from "react";
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
 import FastApi from "../../api/FastApi";
+import FastApiErrorMessage from "./FastApiErrorMessage";
 
 function Home() {
   const [toDoList, set_toDoList] = useState([]);
+  const [errorDetail, set_errorDetail] = useState({detail:[]});
 
   const navigate = useNavigate();
 
   const userId = process.env.REACT_APP_TEST_USER_ID;
 
   function getToDoList() {
-    FastApi("get",`api/todo/list/${userId}`,null,(json)=>{set_toDoList([...json])},(json)=>{console.log(json)})
+    FastApi("get",`api/todo/list/${userId}`,null,(json)=>{set_toDoList([...json])},null)
   }
 
   function listingToDo() {
     return (
       toDoList.map((toDo) => {
         return (
-        <Fragment key={toDo}>
+        <Fragment key={toDo.id}>
           <li>
             <Link to={"/" + toDo.id}>
               {toDo.todo_name}
@@ -32,9 +34,54 @@ function Home() {
 
   useEffect(() => {getToDoList()},[]);
 
+  const [ToDo_New_name,set_ToDo_New_name] = useState("")
+  const [ToDo_New_text,set_ToDo_New_text] = useState("")
+
+  function createTodo(event) {
+    event.preventDefault()
+
+    FastApi("POST",`api/todo/create/${userId}`,{user_id:userId, todo_name:ToDo_New_name, text:ToDo_New_text},
+    () => {
+      set_ToDo_New_name("")
+      set_ToDo_New_text("")
+
+      getToDoList()
+    }
+    ,(json) => {
+      set_errorDetail(json);
+    })
+  }
+
+  function set_ToDo_New_nameChange(event) {
+    set_ToDo_New_name(event.target.value)
+  }
+  function set_ToDo_New_textChange(event) {
+    set_ToDo_New_text(event.target.value)
+  }
+
+  function todoForm() {
+    return (
+      <div className="mt-4">
+        <div className="d-flex">
+          <div className="w-100">
+            <div className="d-flex">
+              <div className="flex-grow-1">
+                <input value={ToDo_New_name} onChange={set_ToDo_New_nameChange} className="flex-grow-1 form-control mb-1" placeholder="Title"/>
+                <input value={ToDo_New_text} onChange={set_ToDo_New_textChange} className="flex-grow-1 form-control" placeholder="content"/>
+              </div>
+              <button onClick={createTodo} className="btn btn-primary ms-3">추가</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="text-center">
-      <ul>
+    <div className="text-center container">
+      {todoForm()}
+      {FastApiErrorMessage(errorDetail)}
+      <ul className="list-unstyled">
         {listingToDo()}
       </ul>
     </div>
