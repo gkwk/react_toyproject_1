c1df3e8b43453247633addf9543d1195cedadcef
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-17 01:05:44 +0900

    [feat] 네비게이션 바 수정

--- src/components/jsx/Header.jsx
+++ src/components/jsx/Header.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const isNavBarToggled = useRef()
+  
+  const [navBarToggle, set_navBarToggle] = useState(false)
+
+  useEffect(() => {
+    if (isNavBarToggled.current.className.toLowerCase().includes("show")) {
+      set_navBarToggle(true)
+    }
+
+    isNavBarToggled.current.addEventListener('hidden.bs.collapse', (event) => {
+      set_navBarToggle(false)
+    })
+
+    isNavBarToggled.current.addEventListener('show.bs.collapse', (event) => {
+      set_navBarToggle(true)
+    })
+  },[])
+
     return (
 			<nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
 
@@ -16,12 +35,12 @@ function Header() {
 						aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon" />
 				</button>
-				<div className="collapse navbar-collapse ">
+				<div className="collapse navbar-collapse" id="navbarSupportedContent" ref={isNavBarToggled}>
 						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-								<li className="nav-item me-2">
+								<li className={`nav-item ${navBarToggle && "ms-auto"} ps-2`}>
 								<Link to={"/register"} className="navbar-link">회원가입</Link>
 								</li>
-								<li className="nav-item">
+								<li className={`nav-item ${navBarToggle && "ms-auto"} ps-2`}>
 								<Link to={"/login"} className="navbar-link">로그인</Link>
 								</li>
 						</ul>
