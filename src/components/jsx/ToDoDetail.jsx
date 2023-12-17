648b8db9a8f37093dcdb11544b66f5787cb59b48
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-17 20:52:21 +0900

    [feat] todo 상세 정보 페이지를 api 함수에 맞게 수정

--- src/components/jsx/ToDoDetail.jsx
+++ src/components/jsx/ToDoDetail.jsx
@@ -4,50 +4,45 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import moment from 'moment/min/moment-with-locales'
 
 import FastApi from '../../api/FastApi';
+import Header from './Header';
 
 function ToDoDetail() {
   const { id } = useParams();
 
-  const [toDoDetail, set_toDoDetail] = useState([]);
+  const [toDoDetail, set_toDoDetail] = useState({});
 
   const navigate = useNavigate();
-
-  const userId = localStorage.getItem("id");
 
   moment.locale('ko');
 
   function getToDoDetail() {
     FastApi(
       'get',
-      `api/todo/detail/${userId}/${id}`,
+      `api/todo/detail/${id}`,
+      null,
       null,
       (json) => {
-        set_toDoDetail([...json]);
+        set_toDoDetail({...json});
       },
       null,
+      true
     );
   }
 
   function detailToDo() {
     return (
-      <Fragment>
-        {toDoDetail.map((todo) => {
-          return (
-            <Fragment key={todo.id}>
-              <h2 className="border-bottom py-2">{todo.todo_name}</h2>
-              <div className="card my-3">
-                <div className="card-body">
-                  <div className="card-text">{todo.text}</div>
-                  <div className="d-flex justify-content-end">
-                    <div className="badge bg-light text-dark p-2">
-                      {moment(todo.create_date).format("YYYY년 MM월 DD일 hh:mm a")}
-                    </div>
-                  </div>
-                </div>
+      <Fragment key={toDoDetail.id}>
+        <h2 className="border-bottom py-2">{toDoDetail.todo_name}</h2>
+        <div className="card my-3">
+          <div className="card-body">
+            <div className="card-text">{toDoDetail.text}</div>
+            <div className="d-flex justify-content-end">
+              <div className="badge bg-light text-dark p-2">
+                {moment(toDoDetail.create_date).format("YYYY년 MM월 DD일 hh:mm a")}
               </div>
-            </Fragment>
-          );
-        })}
+            </div>
+          </div>
+        </div>
       </Fragment>
     );
   }
@@ -57,15 +52,18 @@ function ToDoDetail() {
   }, []);
 
   return (
-    <div className="container">
-      {detailToDo()}
+    <Fragment>
+      <Header />
+      <div className="container">
+        {detailToDo()}
 
-      <div className="text-center">
-        <Link to={'/'}>
-          <button className="btn btn-primary">Back</button>
-        </Link>
+        <div className="text-center">
+          <Link to={'/'}>
+            <button className="btn btn-primary">Back</button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
