3e59d6ba91ee0c01e415200dd1c9ef9b76327419
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-17 20:52:49 +0900

    [feat] 404 페이지를 api 함수에 맞게 수정

--- src/components/jsx/NotFound.jsx
+++ src/components/jsx/NotFound.jsx
@@ -1,13 +1,19 @@
+import { Fragment } from "react";
+import Header from "./Header";
+
 function NotFound() {
   return (
-    <div className="text-center">
-      <h1>
-        <b>404</b>
-      </h1>
-      <h1>
-        <b>Not Found</b>
-      </h1>
-    </div>
+    <Fragment>
+      <Header />
+      <div className="text-center">
+        <h1>
+          <b>404</b>
+        </h1>
+        <h1>
+          <b>Not Found</b>
+        </h1>
+      </div>
+    </Fragment>
   );
 }
 
