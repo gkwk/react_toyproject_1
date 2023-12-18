4e3564c3f6aed650c27c83643f8e5d866540d171
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-18 23:10:50 +0900

    [feat] todo 완료여부 수정 기능 추가

--- src/components/jsx/ToDoDetail.jsx
+++ src/components/jsx/ToDoDetail.jsx
@@ -6,10 +6,19 @@ import moment from 'moment/min/moment-with-locales';
 import FastApi from '../../api/FastApi';
 import Header from './Header';
 
+import "../css/Custom.css"
+
 function ToDoDetail() {
   const { id } = useParams();
 
   const [toDoDetail, set_toDoDetail] = useState({});
+  const [toDoIsFinished, set_toDoIsFinished] = useState(false);
+
+  function changeToDoIsFinished(event) {
+    set_toDoIsFinished(() => (event.target.checked))
+    const params = { todo_name: toDoDetail.todo_name, text: toDoDetail.text, todoId: id, is_finished : event.target.checked };
+    updateTodo(params);
+  }
 
   const navigate = useNavigate();
 
@@ -29,13 +38,48 @@ function ToDoDetail() {
     );
   }
 
+  function updateTodo(params) {
+    FastApi(
+      'PUT',
+      `api/todo/update`,
+      null,
+      params,
+      (json) => {
+      },
+      (json) => {
+        set_errorDetail(json);
+      },
+			true
+    );
+  }
+
+  function deleteTodo(event) {
+    event.preventDefault();
+
+    FastApi(
+      'DELETE',
+      `api/todo/delete`,
+      null,
+      { todoId: id },
+      (json) => {
+        navigate(`/`)
+      },
+      (json) => {
+        set_errorDetail(json);
+      },
+			true
+    );
+  }
+
   function detailToDo() {
     return (
       <Fragment key={toDoDetail.id}>
-        <h2 className="border-bottom py-2">{toDoDetail.todo_name}</h2>
+        <h2 className="border-bottom py-2 custom-title">{toDoDetail.todo_name}</h2>
         <div className="card my-3">
           <div className="card-body">
-            <div className="card-text">{toDoDetail.text}</div>
+            <div className="d-flex justify-content-end">
+              <input  type="checkbox" checked={toDoIsFinished || false} onChange={changeToDoIsFinished} style={{width:"50px",height:"50px"}}/>
+            </div>
             <div className="d-flex justify-content-end">
               <div className="badge bg-light text-dark p-2">
                 {moment(toDoDetail.create_date).format(
@@ -50,6 +94,7 @@ function ToDoDetail() {
                 )}
               </div>
             </div>
+            <div className="card-text">{toDoDetail.text}</div>
           </div>
         </div>
       </Fragment>
@@ -60,21 +105,31 @@ function ToDoDetail() {
     getToDoDetail();
   }, []);
 
+  useEffect(() => {
+    set_toDoIsFinished(toDoDetail.is_finished);
+  }, [toDoDetail]);
+
   return (
     <Fragment>
       <Header />
       <div className="container">
         {detailToDo()}
 
-        <div className="text-center">
-          <Link to={`/todo/edit/${id}`}>
-            <button className="btn btn-primary">Edit</button>
-          </Link>
-
-          <Link to={'/'} className='ps-3'>
-            <button className="btn btn-primary">Back</button>
-          </Link>
-        </div>
+        <ul className="d-flex flex-nowrap justify-content-center list-unstyled">
+          <li>
+            <Link to={`/todo/edit/${id}`}>
+              <button className="btn btn-primary">Edit</button>
+            </Link>
+          </li>
+          <li className='ps-2'>
+            <button className="btn btn-primary" onClick={deleteTodo}>Delete</button>
+          </li>
+          <li className='ps-2'>
+            <Link to={'/'}>
+              <button className="btn btn-primary">Back</button>
+            </Link>
+          </li>
+        </ul>
       </div>
     </Fragment>
   );
